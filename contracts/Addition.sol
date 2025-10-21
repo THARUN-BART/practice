4f1473d5ddf554c0ecadb323f48ae9826c10da9e
THARUN <tharunpoongavanam@gmail.com>
2025-10-22 04:23:28 +0530

    My First contract in BlockChain

---
+++ contracts/Addition.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Addition {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+}
